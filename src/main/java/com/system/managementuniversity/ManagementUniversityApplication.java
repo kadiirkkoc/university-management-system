8a33d759fcc6c0c99c4ac583eba94b68eac43657
Kadir Koç <kadiirkkoc@gmail.com>
2023-08-14 21:43:22 +0200

    initial commit

---
+++ src/main/java/com/system/managementuniversity/ManagementUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.system.managementuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/managementuniversity/ManagementUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.managementuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
